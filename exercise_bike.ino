a385fdc0bcf663e170756487c4b5d4ffba0e1627
dwesely <dwesely@users.noreply.github.com>
2023-01-13 06:54:18 -0500

    Planning

---
+++ exercise_bike.ino
@@ -0,0 +1,38 @@
+
+/*
+ * display_workout(mode)
+ * Convert mode to TM1637 segments 
+ * return segments to display
+ */
+
+void setup() {
+  // put your setup code here, to run once:
+
+  /*
+   * Select workout
+   * Loop until encoder button pressed
+   */
+
+   /*
+    * Select workout time
+    * Loop until encoder button pressed
+    */
+
+    /*
+     * Select max speed
+     * Loop until encoder button pressed
+     */
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  /*
+   * Check time
+   * Check rotation count
+   * Calculate speed
+   * Display speed
+   */
+
+}
